8032e2f43168fe8ba16ce9b041e4fcb4159bb3c9
payal bhattamisra <payalbhattamisra@gmail.com>
2025-04-10 18:09:40 +0530

    vbnm

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+    document.body.classList.toggle('dark');
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to Payal's modern portfolio 🌟");
+  });
+  
